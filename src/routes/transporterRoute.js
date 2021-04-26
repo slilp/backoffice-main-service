cc274580d55eb01cc3877eb993020e6178289e7e
6kamens <slil.puangpoom@gmail.com>
2021-04-27 00:26:31 +0700

    Remove ignored files

--- src/routes/transporterRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const transporterController = require('../controllers/transporterController');
-
-router.post('/add', transporterController.insertTransporter);
-router.get('/all', transporterController.getAllTransporterList);
-router.delete('/delete/:tid', transporterController.deleteTransporter);
-
-module.exports = router;
